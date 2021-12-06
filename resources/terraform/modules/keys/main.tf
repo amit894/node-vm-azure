2ab27d9f5e8f8f2572a30bac093852aff207e785
Amit Raj <amit.894@gmail.com>
2021-12-06 14:02:33 +0530

    Adding Keys Module

--- resources/terraform/modules/keys/main.tf
+++ resources/terraform/modules/keys/main.tf
@@ -3,7 +3,3 @@ resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
-output "tls_private_key" {
-    value = tls_private_key.example_ssh.private_key_pem
-    sensitive = true
-}
